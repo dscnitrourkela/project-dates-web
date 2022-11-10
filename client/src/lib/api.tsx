57a0d731ec402157a6bc54c704b16070526e990c
Ritesh Patil <riteshsp2000@gmail.com>
2022-11-10 22:56:14 +0530

    fix: update the api endpoints

--- client/src/lib/api.tsx
+++ client/src/lib/api.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Configure and an instance of axios
 export const avenueApi = axios.create({
-  // baseURL: 'https://avenue-api.nitrkl.in',
-  baseURL: 'http://localhost:9000',
+  // baseURL: 'http://localhost:9000',
+  baseURL: 'https://avenue-api.nitrkl.in',
   timeout: 60000,
   timeoutErrorMessage: 'Request Timeout: Please try again',
 });
